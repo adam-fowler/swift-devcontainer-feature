39019f4c6003e8c679933a05b7be472fe4810495
Adam Fowler <adamfowler71@gmail.com>
2023-03-16 08:15:23 +0000

    Update test

--- test/swiftly/test.sh
+++ test/swiftly/test.sh
@@ -37,7 +37,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "execute command" bash -c 'swiftly --version'
+check "execute command" bash -c 'swift --version'
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
