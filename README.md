873485c8339125c4f32c57f244a854d271df0bbf
Adam Fowler <adamfowler71@gmail.com>
2023-03-16 07:31:10 +0000

    swiftly devcontainer feature

---
+++ README.md
@@ -0,0 +1,14 @@
+# Swift Dev Container Feature
+
+A feature for a dev container that installs the Swift using swiftly.
+
+Example using the feature:
+
+```jsonc
+{
+    "image": "ubuntu:jammy",
+    "features": {
+        "ghcr.io/adam-fowler/swift-devcontainer-feature/swiftly:0": { }
+    }
+}
+```
---
+++ src/swiftly/install.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+# The 'install.sh' entrypoint script is always executed as the root user.
+#
+# These following environment variables are passed in by the dev container CLI.
+# These may be useful in instances where the context of the final 
+# remoteUser or containerUser is useful.
+# For more details, see https://containers.dev/implementors/features#user-env-var
+# echo "The effective dev container remoteUser is '$_REMOTE_USER'"
+# echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
+
+# echo "The effective dev container containerUser is '$_CONTAINER_USER'"
+# echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
+STARTDIR=$(pwd)
+TEMPDIR=$(mktemp -d)
+VERSION=${VERSION:-"default"}
+
+cleanup()
+{
+    cd "$STARTDIR"
+    if [ -n "$TEMPDIR" ]; then
+        rm -rf "$TEMPDIR"
+    fi
+}
+
+trap cleanup EXIT $?
+
+# swiftly requires curl
+apt install curl
+
+echo "Downloading patrickfreed/swiftly"
+git clone https://github.com/patrickfreed/swiftly "$TEMPDIR"
+cd "$TEMPDIR"
+git checkout bootstrap
+
+export SWIFTLY_BIN_DIR=/usr/local/bin
+source ./install/swiftly-install.sh -y
+
+swiftly install latest
+swiftly use latest
